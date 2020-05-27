f3595f28526485fc08a4af7476431a7d5e036a2d
javierpayan <60956433+javierpayan@users.noreply.github.com>
2020-05-26 22:17:57 -0600

    Add files via upload

---
+++ Final_Proyect.py
@@ -0,0 +1,50 @@
+import RPi.GPIO as GPIO
+import socket
+import time
+from datetime import datetime
+import cec
+import threading
+host = "192.168.1.66"
+port = 5000 
+cec.init()
+command = ''
+tv=cec.Device(cec.CECDEVICE_TV)
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(7, GPIO.IN)         #Read output from PIR motion sensor
+value = 0
+def speech_recog():
+    while True: 
+         # Create socket
+        s = socket.socket()                                
+    # Connect to host
+        s.connect((host, port))
+
+    # Receive command voice 
+        command = s.recv(1024)
+        command = command.decode()     
+        if command =="turn on":
+            tv.power_on()
+        if command =="turn off":
+            tv.standby()
+        if command == "disconnect":
+            s.close()
+            print("Disconected from server")
+            t1.join()
+            t2.join()
+            break
+    
+        s.close()
+
+def movement():
+    while True:
+        i = GPIO.input(7)
+        if i==1:
+            tv.standby()
+threads = []
+t1 = threading.Thread(target=speech_recog)
+threads.append(t1)
+t1.start()
+t2 = threading.Thread(target=movement)
+threads.append(t2)
+t2.start()
---
+++ sockets_server.py
@@ -0,0 +1,48 @@
+import speech_recognition as sr
+import socket  
+import time     
+
+host = "192.168.1.66"
+port = 5000 
+
+conn = None
+addr = None
+
+r1 = sr.Recognizer()
+
+# Create socket
+s = socket.socket()             
+s.bind((host, port))            
+s.listen()          
+print ("Server listening....")
+
+while True:
+
+    # Accept connection
+    conn, addr = s.accept()     
+    print ("Got connection from", addr)
+
+    #Audio recognition
+    try:
+        with sr.Microphone() as source:
+            audio = r1.listen(source)
+        if 'turn on' in r1.recognize_google(audio):
+            print("turn on")
+            msg = 'turn on'  
+            conn.send(msg.encode())      
+        elif 'turn off' in r1.recognize_google(audio):
+            print("turn off")
+            msg = 'turn off'  
+            conn.send(msg.encode()) 
+        elif 'disconnect' in r1.recognize_google(audio):
+            print("disconnect")
+            msg = 'disconnect'  
+            conn.send(msg.encode())
+            conn.close()
+            break
+        else:
+            conn.send("Command not available.".encode())
+            print("Command not available")
+    except:
+        print("command not recognized")
+    conn.close()
